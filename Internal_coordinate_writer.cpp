4a14c5248afd285246015b2f4acbd71fb186143c
CaryD <122555052+PhaedrusD@users.noreply.github.com>
2025-02-18 10:51:07 -0600

    Update Internal_coordinate_writer.cpp
    This program writes the internal coordinates to a file, used in conjunction with the .str file from the CGENFF website, automates the topology writing process for the CHARMM force field. I run my programs on a MAC, but CGENFF allows use of their program for free to universities, however their code only works on Linux machines. I am writing an alternative program for this purpose on a different computer that will include the CGENFF program, essentially automating the entire process

--- Internal_coordinate_writer.cpp
+++ Internal_coordinate_writer.cpp
@@ -162,7 +162,7 @@ double dihedral(const Atom& atom1, const Atom& atom2, const Atom& atom3, const A
     double phiRad = acos(cosAngle);
 
     // Determine the sign of phi based on the sign of signDot
-    double signDot = normal1[0] * vec3[0] + normal1[1] * vec3[1] + normal1[2] * vec3[2];
+    double signDot = normal1[0] * vec4[0] + normal1[1] * vec4[1] + normal1[2] * vec4[2];
     double phiDeg;
     if (signDot <= 0) {
         phiDeg = phiRad * 180.0 / M_PI;
@@ -179,22 +179,24 @@ double dihedral(const Atom& atom1, const Atom& atom2, const Atom& atom3, const A
 void writeInternalCoordinateTable(const Atom atoms[], int numAtoms, std::ostream& out) {
     // Loop through combinations of four atoms to calculate internal coordinates
     for (int i = 0; i < numAtoms - 3; ++i) {
-        for (int j = i + 1; j < numAtoms - 2; ++j) {
-            for (int k = j + 1; k < numAtoms - 1; ++k) {
-                for (int l = k + 1; l < numAtoms; ++l) {
+        for (int j = 0; j < numAtoms - 2; ++j) {
+            for (int k = 0; k < numAtoms - 1; ++k) {
+                for (int l = 0; l < numAtoms; ++l) {
                     // Calculate distances and angles
                     double distAB = Distance(atoms[i], atoms[j]);
                     double angleABC = Angle(atoms[i], atoms[j], atoms[k]);
                     double dihedralABCD = dihedral(atoms[i], atoms[j], atoms[k], atoms[l]);
                     double angleBCD = Angle(atoms[j], atoms[k], atoms[l]);
                     double distCD = Distance(atoms[k], atoms[l]);
+                    double distBC = Distance(atoms[j], atoms[k]);
 
                     // Debug prints
                     std::cout << "distAB: " << distAB << " angleABC: " << angleABC << " dihedralABCD: " << dihedralABCD << " angleBCD: " << angleBCD << " distCD: " << distCD << std::endl;
 
 
                     // Check if distances are less than or equal to 3
-                    if (distAB <= 2.0 && distCD <= 2.0) {
+                    if (distAB < 2.1 && distBC < 2.1 && distCD < 2.1 && distAB * distBC * distCD != 0 && !std::isnan(angleABC) && 
+    !std::isnan(dihedralABCD) && !std::isnan(angleBCD)) {
                         // Write internal coordinate table entry to output stream
                         out << "IC " << atoms[i].type << " " << atoms[j].type << " " << atoms[k].type << " " << atoms[l].type << "  " << distAB << " " << angleABC << "  " << dihedralABCD << " " << angleBCD << "  " << distCD << std::endl;
                     }
@@ -245,13 +247,13 @@ void insertTopologyInfo(const string& pdbFilename, const vector<ResidueInfo>& to
 
 // Main function
 int main() {
-    std::string residueFilename = "*****.str";  // File containing topology information
-    string pdbFilename = "topology_IC.inp";  // Original PDB file name
+    std::string residueFilename = "ori_priority_allh.str";  // File containing topology information
+    string pdbFilename = "top_all27_prot_na_LYS.inp";  // Original PDB file name
 
     Atom atoms[SIZE];  // Array to store atom data
 
     // Read atom data from PDB file
-    int numAtoms = readData("*****.pdb", atoms);
+    int numAtoms = readData("ori_priority_allh.pdb", atoms);
     std::cout << "Number of atoms read: " << numAtoms << std::endl;
 
     // Read topology information from file
