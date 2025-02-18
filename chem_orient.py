5494eff77d5a18c529044ca2416b04f99904f07c
CaryD <122555052+PhaedrusD@users.noreply.github.com>
2025-02-18 11:04:27 -0600

    Create chem_orient.py
    This code is intended to orient and place the chemical modification using the terminal alkyne on the novel amino acid and the azide on the incoming "unknown" molecule.

---
+++ chem_orient.py
@@ -0,0 +1,37 @@
+import numpy as np
+import MDAnalysis as mda
+
+# Load the first and second protein structures
+u1 = mda.Universe("patch_YCLK.pdb")  # First protein (reference)
+u2 = mda.Universe("azide2.pdb")      # Second protein (target)
+
+# Select the specific atoms for translation
+c_atom = u1.select_atoms("resid 139 and name C33")  # C-terminus carbon in the first protein
+n_atom = u2.select_atoms("resid 1 and name N11")    # N-terminus nitrogen in the second protein
+
+# Ensure both selections are valid
+if len(c_atom) != 1 or len(n_atom) != 1:
+    raise ValueError("Atom selections failed. Check residue IDs or atom names.")
+
+# Get the coordinates of the selected atoms
+c_coords = c_atom.positions[0]  # Coordinates of the C atom
+n_coords = n_atom.positions[0]  # Coordinates of the N atom
+
+# Calculate the translation vector to align N to C
+translation_vector = c_coords - n_coords
+
+# Translate the entire second protein
+u2.atoms.translate(translation_vector)
+
+# Translate the second protein 6 Å along the x-axis
+translation_6x = np.array([-6.0, 0.0, 0.0])  # 6 Å shift in the x-direction
+u2.atoms.translate(translation_6x)
+
+# Write the transformed coordinates of the second protein
+u2.atoms.write("C2.pdb")
+
+# Write the coordinates of the first protein
+u1.atoms.write("C1.pdb")
+
+
+print("Files written: C1.pdb, C2.pdb")
